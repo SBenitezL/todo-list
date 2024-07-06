bd4f99b84540ec4b3360892a0f8656e2d7c713a8
sBenitezL <sbenitez1607@gmail.com>
2024-07-06 15:20:46 -0500

    Add: own exception bad credentials error.

--- src/main/java/com/sbenitez/app/infraestructure/exception/own/BadCredentialException.java
+++ src/main/java/com/sbenitez/app/infraestructure/exception/own/BadCredentialException.java
@@ -1,5 +1,36 @@
 package com.sbenitez.app.infraestructure.exception.own;
 
-public class BadCredentialException {
+import com.sbenitez.app.infraestructure.exception.structure.ErrorCode;
 
+import lombok.Getter;
+
+@Getter
+public class BadCredentialException extends RuntimeException {
+    /** Personalize menssage error. */
+    private final String messageKey;
+    /** Personalize code error. */
+    private final String code;
+
+    /**
+     * BadCredentialException, use the values define on enum {@code ErrorCode}.
+     * 
+     * @param code Error code to use on exception define on enum {@code ErrorCode}.
+     */
+    public BadCredentialException(ErrorCode code) {
+        super(code.getCode());
+        this.messageKey = code.getMessageKey();
+        this.code = code.getCode();
+    }
+
+    /**
+     * Use the default value define to BadCredentialException on enum
+     * {@code ErrorCode} and add a personalize message to show the error.
+     * 
+     * @param message Personalized message error.
+     */
+    public BadCredentialException(final String message) {
+        super(message);
+        this.messageKey = ErrorCode.BAD_CREDENTIALS.getMessageKey();
+        this.code = ErrorCode.BAD_CREDENTIALS.getCode();
+    }
 }
